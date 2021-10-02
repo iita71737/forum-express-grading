44bb0b96a2038bbfd1640d7b1f0877cf9ab4c29c
iita71737 <iita71737@gmail.com>
2021-10-02 15:26:52 +0800

    version1

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 app.listen(port, () => {
