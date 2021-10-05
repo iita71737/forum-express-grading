07bb81f128d817f098610db3d31c8bc5d904793e
iita71737 <iita71737@gmail.com>
2021-10-05 17:11:10 +0800

    added dashboard

--- controllers/restController.js
+++ controllers/restController.js
@@ -62,6 +62,39 @@ const restController = {
                 restaurant: restaurant.toJSON()
             })
         })
+    },
+    getFeeds: (req, res) => {
+        return Promise.all([
+            Restaurant.findAll({
+                limit: 10,
+                raw: true,
+                nest: true,
+                order: [['createdAt', 'DESC']],
+                include: [Category]
+            }),
+            Comment.findAll({
+                limit: 10,
+                raw: true,
+                nest: true,
+                order: [['createdAt', 'DESC']],
+                include: [User, Restaurant]
+            })
+        ]).then(([restaurants, comments]) => {
+            return res.render('feeds', {
+                restaurants: restaurants,
+                comments: comments
+            })
+        })
+    },
+    getDashboard: (req, res) => {
+        return Restaurant.findByPk(req.params.id, {
+            include: [
+                Category,
+                { model: Comment, include: [User] }
+            ]
+        }).then(restaurant => {
+            return res.render('dashboard', { restaurant: restaurant.toJSON() })
+        })
     }
 }
 module.exports = restController
--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,11 @@ module.exports = (app, passport) => {
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
   app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
+
+  app.get('/restaurants/feeds', authenticated, restController.getFeeds)
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+  app.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+
   app.post('/comments', authenticated, commentController.postComment)
   app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
   app.get('/users/:id', authenticated, userController.getUser)
